6c2b7ee564d14ee0cada84c0ab9986aee6579c1e
Brittany L. Walentin <brittany.walentin@gmail.com>
2015-06-22 08:39:32 -0700

    Clock broken. Other procs/methods working I believe.

--- ch14_blocks&Procs/clock.rb
+++ ch14_blocks&Procs/clock.rb
@@ -1,5 +1,5 @@
 grandfather = Proc.new do |hour|
-  puts "DONG"*hour
+  puts "DONG"
 end
 
 def chiming some_proc
@@ -10,6 +10,40 @@ def chiming some_proc
   else
     chimes = Time.new.hour
   end
+  some_proc.call * chimes
 end
 
 chiming grandfather
+
+puts " ---------------------------"
+
+def do_self_importantly some_proc
+  puts "Everybody just HOLD ON! I'm doing something..."
+  some_proc.call
+  puts "OK everyone, I'm done. As you were."
+end
+
+say_hello = Proc.new do
+  puts 'my name is mrs. bigshot.'
+end
+
+say_goodbye = Proc.new do
+  puts 'goodbye'
+end
+  do_self_importantly say_hello
+  do_self_importantly say_goodbye
+
+puts " ---------------------------"
+
+def my_method some_proc
+  puts "I'm in the method."
+  some_proc.call
+  puts "I'm in the method sandwich."
+end
+
+say = Proc.new do
+  puts "This is my proc."
+end
+
+# method/proc call:
+my_method say
